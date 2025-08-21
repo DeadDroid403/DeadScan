dc06d5b3a1b2a5d4e43c608bd3346606b3a72f49
DeadDroid <deaddroid403@gmail.com>
2025-08-21 19:45:44 +0530

    added riliability scan

--- DeadScan.py
+++ DeadScan.py
@@ -4,15 +4,79 @@ import argparse
 import sys
 from threading import Thread
 import time
+from rich.console import Console
+import time
+import random 
 
-class DeadScan:
+class Decoration:
+    console = Console()
+    def __init__(self):
+        """
+            Constructor Function to Initialize attributes and Constants.
+        """
+        self.is_decoration = True 
+        self.console = Console()
+        self.color = ["#FF0000","#FF7F00","#FFFF00","#00FF00","#00FFFF","#0000FF","#8B00FF","#FF1493","#FFD700","#00FF7F","#1E90FF","#FF4500"]
+
+
+    def runRocket(self):
+        rocket_length = 66
+        color = str(random.choice(self.color))
+        for i in range(rocket_length):
+            if i%3==0:
+                color = str(random.choice(self.color))
+            rocket_design = f'[bold {color}]~~==>[/bold {color}]'
+            time.sleep(0.020)
+            if i != (rocket_length-1):
+                self.console.print(" "*i + rocket_design, end='\r')
+            else:   
+                self.console.print(" "*i + rocket_design)
+
+    def runLine2(self):
+        line_length = 70
+        color = str(random.choice(self.color))
+        for i in range(line_length):
+            color = str(random.choice(self.color))
+            line_char = f'[bold {color}]^[/bold {color}]'
+            if i != (line_length-1):
+                self.console.print(line_char , end='')
+            else:   
+                self.console.print(line_char )
+
+
+    def runLine(self):
+        line_length = 70
+        color = "#FF4500"
+        self.console.print(f'[bold {color}]{"-"*line_length}[/bold {color}]')
+
+
+    def logo(self):
+        print("""
+██████╗ ███████╗ █████╗ ██████╗     ███████╗ ██████╗ █████╗ ███╗   ██╗
+██╔══██╗██╔════╝██╔══██╗██╔══██╗    ██╔════╝██╔════╝██╔══██╗████╗  ██║
+██║  ██║█████╗  ███████║██║  ██║    ███████╗██║     ███████║██╔██╗ ██║
+██║  ██║██╔══╝  ██╔══██║██║  ██║    ╚════██║██║     ██╔══██║██║╚██╗██║
+██████╔╝███████╗██║  ██║██████╔╝    ███████║╚██████╗██║  ██║██║ ╚████║
+╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝     ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+    
+Author:     DeadDroid
+Version:    1.3                                                                   
+            """)
+
+
+
+class DeadScan(Decoration):
     def __init__(self):
         """
             Constructor Function to Initialize variables.
         """
+        super().__init__()
         self.openports = []
         self.open_udp_port_filtered = []
+        self.allow_print = 1
         self.thrds = []
+        self.logo()
+
 
     def scanone(self,port):
         """
@@ -25,8 +89,9 @@ class DeadScan:
             result = s.connect_ex((self.ip,port))
             # Checking if Port is open or close.
             if result == 0:
-               print(f"[+] Port {port} is OPEN (got response) ")
-               self.openports.append(port)
+                if self.allow_print:
+                    self.console.print(f"[bold #00FF7F][+] Port {port} is OPEN (got response)[/bold #00FF7F]")
+                self.openports.append(port)
             s.close() # Closing the Socket.
         except Exception as e:
             pass # Simply Ignoring The Exceptions.
@@ -42,7 +107,8 @@ class DeadScan:
             s.connect((self.ip,port))
             s.send(b"") # Sending Empty Payload to UDP. 
             data = s.recv(1024)  # if we recieve data then the port is open. 
-            print(f"[+] Port {port} is OPEN (got response)")
+            if self.allow_print:
+                self.console.print(f"[bold #00FF7F][+] Port {port} is OPEN (got response)[/bold #00FF7F]")
             self.openports.append(port)
         except socket.timeout: # if timeouts then port is probably open or filtered. 
             self.open_udp_port_filtered.append(port)
@@ -58,11 +124,8 @@ class DeadScan:
         # Loop To Scan All Ports Given By User.
         if self.port_type == "T":
             scanning_port = self.scanone
-        elif self.port_type == "U":
-            scanning_port = self.scanoneudp
         else:
-            print(f"[-] The -p flag only supports T or U for TCP or UDP respectively")
-            sys.exit(1)
+            scanning_port = self.scanoneudp
         for port in range (self.start, (self.end + 1)):
             th = Thread(target=scanning_port, args=(port,)) # Creating Thread.
             self.thrds.append(th) # Appending Threads In a List.
@@ -117,6 +180,18 @@ class DeadScan:
                                  dest="port_type",
                                  metavar='',
                                  default="T")
+        self.parser.add_argument("-r","--reliability",
+                                 help="After the initial scan, initiate the riliability scans in bg [def = 0]",
+                                 dest="riliability",
+                                 metavar='',
+                                 type=int,
+                                 default=0)
+        self.parser.add_argument("-rt",
+                                 help="Number of Threads used in riliability scans [def = 100]",
+                                 dest="riliability_threads",
+                                 metavar='',
+                                 type=int,
+                                 default=100)
         self.args = self.parser.parse_args() # Parsing All Args.
 
     def argscheck(self):
@@ -124,7 +199,7 @@ class DeadScan:
             Checking Arguments Provided By User And Raising Error if IP Not
         """
         if len(sys.argv) == 1:
-            self.parser.print_help()
+            self.console.print('[bold #FF0000 ]Error: Target not specified. Please provide an IP address or domain. Use -h for usage details.[/bold #FF0000 ]')
             sys.exit(1)
         else:
             if self.args.ip:
@@ -134,44 +209,97 @@ class DeadScan:
                 self.threads = self.args.threads
                 self.Time = self.args.Time
                 self.port_type = self.args.port_type
+                self.riliability = self.args.riliability
+                self.riliability_threads = self.args.riliability_threads
             else:
-                self.parser.print_help()
+                self.console.print('[bold #FF0000 ]Error: Target not specified. Please provide an IP address or domain. Use -h for usage details.[/bold #FF0000 ]')
                 sys.exit(1)
-        
+            if self.port_type !='T':
+                self.scan_type = 'UDP Scan'
+            elif self.port_type != 'U':
+                self.scan_type = "TCP Scan"
+            else:
+                self.console.print(f"[bold #FF0000 ]Error: Invalid protocol. The -p flag supports only 'T' (TCP) or 'U' (UDP).[/bold #FF0000 ]")
+                sys.exit(1)
+
+    def workOnArgs(self):
+        """
+            Process and handle the arguments provided by the user.
+        """
+        self.runRocket()  # Runs the Rocket 
+        # checking if user asked to display time taken in the initial scan.
+        if self.Time:
+            stime = time.time() # setting the time start point
+            self.console.print(f"""
+[bold #00FF00]------------------------------------------------------------[/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00][bold #FFFF00]Starting Master Scan on Host {self.ip}...[bold #00FF00] ][/bold #00FF00]     
+[bold #00FF00][ [/bold #00FF00]Ports Range: {self.start} - {self.end}[bold #00FF00] ][/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00]Scan Type: {self.scan_type}[bold #00FF00] ][/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00]Threads: {self.threads}[bold #00FF00] ][/bold #00FF00][/bold #FFFF00]
+[bold #00FF00]------------------------------------------------------------[/bold #00FF00]\n""")
+            self.scanloop()
+            # checking if udp filtered ports are opens and printing list of open ports
+            if self.open_udp_port_filtered:
+                if len(self.open_udp_port_filtered) > 50:
+                    self.console.print(f"\n[bold #FF7F00]Filtered UDP ports: {len(self.open_udp_port_filtered)} Entries ! Too big To Display...[/bold #FF7F00]")
+                else:
+                    self.console.print(f"\n[bold #00FF00]Filtered UDP ports {len(self.open_udp_port_filtered)}: {sorted(self.open_udp_port_filtered)}[/bold #00FF00]")
+            # checking if tcp ports are opens and printing list of open ports
+            if self.openports:
+                self.console.print(f"\n[bold #00FF00]Open Ports {len(self.openports)}: {sorted(self.openports)}[/bold #00FF00]")
+            etime = time.time() # setting the time end point
+            # printing ( no ports open ) if both lists are empty
+            if not self.open_udp_port_filtered and not self.openports:
+                self.console.print(f"[bold #FF7F00]Scan result: no open ports detected[/bold #FF7F00]")
+            # printing the time taken in the scan
+            self.console.print(f"\n[bold #00FFFF]Time Taken in This Scan is {etime - stime} Seconds[/bold #00FFFF]")
+            self.runLine() # prints a simple line
+        else:
+            # same functionality as above, just removing the time checking statements
+            self.console.print(f"""
+[bold #00FF00]------------------------------------------------------------[/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00][bold #FFFF00]Starting Master Scan on Host {self.ip}...[bold #00FF00] ][/bold #00FF00]     
+[bold #00FF00][ [/bold #00FF00]Ports Range: {self.start} - {self.end}[bold #00FF00] ][/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00]Scan Type: {self.scan_type}[bold #00FF00] ][/bold #00FF00]
+[bold #00FF00][ [/bold #00FF00]Threads: {self.threads}[bold #00FF00] ][/bold #00FF00][/bold #FFFF00]
+[bold #00FF00]------------------------------------------------------------[/bold #00FF00]\n""")
+            self.scanloop()
+            if self.open_udp_port_filtered:
+                if len(self.open_udp_port_filtered) > 50:
+                    self.console.print(f"\n[bold #FF7F00]Filtered UDP ports: {len(self.open_udp_port_filtered)} Entries ! Too big To Display...[/bold #FF7F00]")
+                else:
+                    self.console.print(f"\n[bold #00FF00]Filtered UDP ports {len(self.open_udp_port_filtered)}: {sorted(self.open_udp_port_filtered)}[/bold #00FF00]")
+            if self.openports:
+                self.console.print(f"\n[bold #00FF00]Open Ports {len(self.openports)}: {sorted(self.openports)}[/bold #00FF00]")
+            if not self.open_udp_port_filtered and not self.openports:
+                self.console.print(f"[bold #FF7F00]Scan result: no open ports detected[/bold #FF7F00]")
+            self.runLine()
+
+        # Processing riliablity scans now
+        if self.riliability:
+            # checking number of riliability scans provided by the user.
+            if self.riliability > 0 and isinstance(self.riliability, int) and self.riliability < 11:
+                self.allow_print = 0  # stops printing open ports while running riliability scans
+                self.threads = self.riliability_threads
+                for i in range(1,(self.riliability+1)):
+                    self.openports = []
+                    self.open_udp_port_filtered = []
+                    self.console.print(f'[bold #FFFF00]Starting riliability scan {i}/{self.riliability} with {self.threads} Threads...[/bold #FFFF00]')
+                    self.scanloop()
+                    if self.open_udp_port_filtered:
+                        if len(self.open_udp_port_filtered) > 50:
+                            self.console.print(f"\n[bold #FF7F00]Filtered UDP ports: {len(self.open_udp_port_filtered)} Entries ! Too big To Display...[/bold #FF7F00]")
+                        else:
+                            self.console.print(f"\n[bold #00FF00]Filtered UDP ports {len(self.open_udp_port_filtered)}: {sorted(self.open_udp_port_filtered)}[/bold #00FF00]")
+                    if self.openports:
+                        self.console.print(f"\n[bold #00FF00]Open Ports {len(self.openports)}: {sorted(self.openports)}[/bold #00FF00]")
+                    if not self.open_udp_port_filtered and not self.openports:
+                        self.console.print(f"[bold #FF7F00]Scan result: no open ports detected[/bold #FF7F00]")
+                    self.runLine()
+
 
 if __name__=="__main__":
     obj = DeadScan()
     obj.arguments()
     obj.argscheck()
-    if obj.ip:
-        if obj.Time:
-            stime = time.time()
-            print("*"*70)
-            print(f"Started Scanning Port From {obj.start} - {obj.end} on IP {obj.ip}")
-            obj.scanloop()
-            if obj.open_udp_port_filtered:
-                if len(obj.open_udp_port_filtered) > 50:
-                    print(f"\nList of Filterded Ports is too big...")
-                else:
-                    print(f"\nList of Filterded Ports = {sorted(obj.open_udp_port_filtered)}")
-            if obj.openports:
-                print(f"\nList of Ports = {sorted(obj.openports)}")
-            etime = time.time()
-            print(f"\nTime Taken in This Scan is {etime - stime} Seconds")
-            print("*"*70)
-        else:
-            print("*"*70)
-            print(f"Started Scanning Port From {obj.start} - {obj.end} on IP {obj.ip}")
-            obj.scanloop()
-            if obj.open_udp_port_filtered:
-                if len(obj.open_udp_port_filtered) > 50:
-                    print(f"\nList of Filterded Ports is too big...")
-                else:
-                    print(f"\nList of Filterded Ports = {sorted(obj.open_udp_port_filtered)}")
-            if obj.openports:
-                print(f"\nList of Ports = {sorted(obj.openports)}")
-            print("*"*70)
-    else:
-        obj.parser.print_help()
-        sys.exit(1)
-        
+    obj.workOnArgs()
